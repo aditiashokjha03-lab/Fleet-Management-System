544b8ca4e7b9e62af5aeb9a31e88c3253936c8bd
Aditi Jha <aditiashokjha03@gmail.com>
2026-01-31 21:03:48 +0530

    Refactor vehicle controller to use Supabase

--- controllers/vehicle.controller.js
+++ controllers/vehicle.controller.js
@@ -1,13 +1,19 @@
+const supabase= require('../config/db');
 const VehicleModel = require('../models/vehicle.model');
 
-const getVehicles = async (req, res) => {
-    try {
-        const vehicles = await VehicleModel.getAll();
-        res.status(200).json(vehicles);
-    }
-    catch (err) {
-        res.status(500).json({message: err.message});
-    }
+exports.getAllVehicles = async (req, res) => {
+    const {data, error} = await supabase.from('vehicles').select('*');
+        if (error) return res.status(500).json({message: err.message});
+        res.status(200).json(data);
 };
 
-module.exports = { getVehicles };
+exports.updateVehicleStatus = async (req,res) => {
+    const {id} = req.params;
+    const {status} = req.body;
+    const {data, error} = await supabase.from('vehicles').update({status}).eq('id', id);
+
+    if (error) return res.status(400).json({error: error.message});
+
+    res.status(200).json({message: "Status updated", data});
+
+};
